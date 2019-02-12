97f126c6e8d4e98316de7335ac79827ed4a98c8b
Michael Chambers <michaelchambers@outlook.ie>
2019-02-12 11:47:58 +0000

    gravatar

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.html import mark_safe
 from django.utils.text import Truncator
+
+from markdown import markdown
+import math
 
 
 
@@ -26,6 +30,25 @@ class Topic(models.Model):
     views = models.PositiveIntegerField(default=0) 
     def __str__(self):
         return self.subject
+
+    def __str__(self):
+        return self.subject
+
+    def get_page_count(self):
+        count = self.posts.count()
+        pages = count / 20
+        return math.ceil(pages)
+
+    def has_many_pages(self, count=None):
+        if count is None:
+            count = self.get_page_count()
+        return count > 6
+
+    def get_page_range(self):
+        count = self.get_page_count()
+        if self.has_many_pages(count):
+            return range(1, 5)
+        return range(1, count + 1)    
     
 
 
@@ -40,3 +63,6 @@ class Post(models.Model):
     def __str__(self):
         truncated_message = Truncator(self.message)
         return truncated_message.chars(30)
+    
+    def get_message_as_markdown(self):
+        return mark_safe(markdown(self.message, safe_mode='escape'))    

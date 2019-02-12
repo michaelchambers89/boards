8dc6bc4712cbe7f011466314d48dc445416168b8
Michael Chambers <michaelchambers@outlook.ie>
2019-02-12 12:06:57 +0000

    end of tut 6, formatted using black

--- myproject/boards/migrations/0002_topic_views.py
+++ myproject/boards/migrations/0002_topic_views.py
@@ -5,14 +5,12 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('boards', '0001_initial'),
-    ]
+    dependencies = [("boards", "0001_initial")]
 
     operations = [
         migrations.AddField(
-            model_name='topic',
-            name='views',
+            model_name="topic",
+            name="views",
             field=models.PositiveIntegerField(default=0),
-        ),
+        )
     ]
--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -7,7 +7,6 @@ from markdown import markdown
 import math
 
 
-
 class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=100)
@@ -19,15 +18,16 @@ class Board(models.Model):
         return Post.objects.filter(topic__board=self).count()
 
     def get_last_post(self):
-        return Post.objects.filter(topic__board=self).order_by('-created_at').first()
+        return Post.objects.filter(topic__board=self).order_by("-created_at").first()
 
 
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
     last_updated = models.DateTimeField(auto_now_add=True)
-    board = models.ForeignKey(Board, on_delete = models.CASCADE, related_name='topics')
-    starter = models.ForeignKey(User, on_delete = models.CASCADE, related_name='topics')
-    views = models.PositiveIntegerField(default=0) 
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name="topics")
+    starter = models.ForeignKey(User, on_delete=models.CASCADE, related_name="topics")
+    views = models.PositiveIntegerField(default=0)
+
     def __str__(self):
         return self.subject
 
@@ -48,21 +48,25 @@ class Topic(models.Model):
         count = self.get_page_count()
         if self.has_many_pages(count):
             return range(1, 5)
-        return range(1, count + 1)    
-    
+        return range(1, count + 1)
+
+    def get_last_ten_posts(self):
+        return self.posts.order_by("-created_at")[:10]
 
 
 class Post(models.Model):
     message = models.TextField(max_length=4000)
-    topic = models.ForeignKey(Topic, on_delete = models.CASCADE, related_name='posts')
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name="posts")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True)
-    created_by = models.ForeignKey(User, on_delete = models.CASCADE, related_name='posts')
-    updated_by = models.ForeignKey(User, on_delete = models.CASCADE, null=True, related_name='+')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    updated_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, related_name="+"
+    )
 
     def __str__(self):
         truncated_message = Truncator(self.message)
         return truncated_message.chars(30)
-    
+
     def get_message_as_markdown(self):
-        return mark_safe(markdown(self.message, safe_mode='escape'))    
+        return mark_safe(markdown(self.message, safe_mode="escape"))
